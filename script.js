0259880ec5c4b6d762983dad20931e93c3f3a3ab
shreyanithin <shreyanithinr@gmail.com>
2024-04-10 23:14:34 +0530

    navbar and video edited

--- script.js
+++ script.js
@@ -1,6 +1,28 @@
 function display(){
-    console.log('clicked');
+//     console.log('clicked');
 
-let check=document.getElementById('menus').classList.add 
-console.log(check);
+// let check=document.getElementsByClassName('menus').classList.add 
+// console.log(check);
+
+
+let get=document.getElementById('menuid');
+if(get.style.display ==="flex")
+{
+    get.style.display="none";
+}
+else{
+    get.style.display="flex";
+}
+
+
+
+
+// document.getElementById('menuid').style.display="flex";
+document.getElementById('head').style.flexDirection="column";
+get.style.flexDirection="column";
+get.style.marginLeft="30px"
+// document.getElementById('ham').style.;
+
+
+
 }
